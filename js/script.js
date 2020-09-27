94db9beb69e91824f5e8ef9cac5e89880ba8df74
Justyna Misiuk <bir8@wp.pl>
2020-09-27 18:54:35 +0200

    second Excersise

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-var computerMove, playerMove;
-playerMove = 'papier';
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'nożyce';
+} else if (randomNumber == '3') {
+  computerMove = 'papier';
+}
+printMessage('Mój ruch: ' + computerMove);
